b72788c3d289ed65d21e7cefdf5dad2531a8ccde
andrew2263 <andrew2263@gmail.com>
2021-10-26 12:36:55 +0300

    card fixes-2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-import './card.js';
+import { createPopupElement } from './card.js';
+import { createOffer, ARRAY_LENGTH } from './data.js';
+
+const offers = Array.from({length: ARRAY_LENGTH}, createOffer);
+const cardTemplate = document.querySelector('#card').content;
+const cardPopup = cardTemplate.querySelector('.popup');
+const mapCanvas = document.querySelector('#map-canvas');
+
+mapCanvas.appendChild(createPopupElement(cardPopup, offers[0]));
